29991bc7c9f5693fe7d05534a00a8056455915f5
duc4nh <duc_anh_nguyen_93@yahoo.com>
2014-10-09 18:05:43 +0100

    Update textUI

--- src/UI/TextUI.java
+++ src/UI/TextUI.java
@@ -12,59 +12,85 @@ import simulator.Simulator;
 
 public class TextUI {
 	public static void main(String[] args) {
-		int input;
+		System.out.println("Welcome to Cat Simulator!");
+		System.out.printf("Before simulation:\n");
+		
+		int input = 1;
+		while (input == 1) {
+		// PRE-SIMULATION
+		Cat cat = inputCat();
+		Environment environment = inputEnvironment();
+		InteractionObject object = inputObject();
+
+		//SIMULATION
+		System.out.println("Start simulation");
+		String result = Simulator.simulation(cat, environment, object);
+		System.out.println(result);
+		
+		//POST-SIMULATION
+		Scanner reader = new Scanner(System.in);
+		System.out.println("New simulation(1)?");
+		input = reader.nextInt();		
+		}
+	}
+	
+	private static Cat inputCat() {
 		Scanner reader = new Scanner(System.in);
 		
-		System.out.println("Welcome to Cat Simulator!");
-		System.out.printf("Before simulator:\n");
-		
-		// QUESTION 1
 		System.out.printf("Choose a preset cat(1) or Define new cat(2):");
-		input = reader.nextInt();
+		int input = reader.nextInt();
 		
 		Cat cat;
 		if (input == 1) {
 			CatDatabase.printAll();
 			//TODO choose 1
+			cat = new Cat();
 		}
 		else {
 			//TODO input information
 			cat = new Cat();
 		}
-			
-		// QUESTION 2
+		
+		return cat;
+	}
+	
+	private static Environment inputEnvironment() {
+		Scanner reader = new Scanner(System.in);
+		
 		System.out.printf("Choose preset environment(1) or Define new environment(2):");
-		input = reader.nextInt();
+		int input = reader.nextInt();
 		
 		Environment environment;
 		if (input == 1) {
 			EnvironmentDatabase.printAll();
 			//TODO choose 1
+			environment = new Environment();
 		}
 		else {
 			//TODO input information
 			environment = new Environment();
 		}
 		
-		// QUESTION 3
+		return environment;
+	}
+
+	private static InteractionObject inputObject() {
+		Scanner reader = new Scanner(System.in);
+		
 		System.out.printf("Choose a preset interaction object(1) or Define new interaction object(2):");
-		input = reader.nextInt();
+		int input = reader.nextInt();
 		
 		InteractionObject object;
 		if (input == 1) {
 			ObjectDatabase.printAll();
 			//TODO choose 1
+			object = new InteractionObject();
 		}
 		else {
 			//TODO input information
 			object = new InteractionObject();
 		}
-			
-		System.out.println("Start simulation");
-		Simulator.simulation(cat, environment, object);
-		System.out.println(input);
-
 		
+		return object;
 	}
-	
 }
--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -2,10 +2,10 @@ package simulator;
 
 public class Simulator {
 
-	public static void simulation(Cat cat, Environment environment,
+	public static String simulation(Cat cat, Environment environment,
 			InteractionObject object) {
 		// TODO Auto-generated method stub
-		
+		return "The cat is chasing the mouse";
 	}
 
 }
