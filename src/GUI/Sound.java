f7ba412eaa548884845291e1eefd09c1531ad85e
Duc Anh Nguyen <duc.nguyen-3@student.manchester.ac.uk>
2015-01-25 22:51:49 +0000

    Minor changes

--- src/GUI/Sound.java
+++ src/GUI/Sound.java
@@ -1,17 +1,16 @@
 package GUI;
 
-import java.io.*;
-
 import sun.audio.*;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 /**
  * Contain methods that play sound in GUI
- * 
+ *
  * @author DucAnh
- * 
  */
-public class Sound
-{
+public class Sound {
 	private ContinuousAudioDataStream music;
 	private AudioData button1;
 	private AudioData button2;
@@ -21,32 +20,26 @@ public class Sound
 	/**
 	 * Constructor
 	 */
-	public Sound()
-	{
-		try
-		{
+	public Sound() {
+		try {
 			AudioData musicData = audioData("src/sound/music1.wav");
 			music = new ContinuousAudioDataStream(musicData);
 			button1 = audioData("src/sound/button1.wav");
 			button2 = audioData("src/sound/button2.wav");
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public boolean isSoundOn()
-	{
+	public boolean isSoundOn() {
 		return soundOn;
 	}
 
-	public boolean isMusicOn()
-	{
+	public boolean isMusicOn() {
 		return musicOn;
 	}
 
-	public void toggleMusic()
-	{
+	public void toggleMusic() {
 		musicOn = !musicOn;
 		if (musicOn)
 			AudioPlayer.player.start(music);
@@ -54,36 +47,29 @@ public class Sound
 			AudioPlayer.player.stop(music);
 	}
 
-	public void musicStop()
-	{
+	public void musicStop() {
 		AudioPlayer.player.stop(music);
 	}
 
-	public void toggleSound()
-	{
+	public void toggleSound() {
 		soundOn = !soundOn;
 	}
 
-	public void playButton1()
-	{
-		if (soundOn)
-		{
+	public void playButton1() {
+		if (soundOn) {
 			AudioDataStream audio = new AudioDataStream(button1);
 			AudioPlayer.player.start(audio);
 		}
 	}
 
-	public void playButton2()
-	{
-		if (soundOn)
-		{
+	public void playButton2() {
+		if (soundOn) {
 			AudioDataStream audio = new AudioDataStream(button2);
 			AudioPlayer.player.start(audio);
 		}
 	}
 
-	private AudioData audioData(String url) throws Exception
-	{
+	private AudioData audioData(String url) throws Exception {
 		// open the sound file as a Java input stream
 		InputStream in = new FileInputStream(url);
 		AudioStream audioStream = new AudioStream(in);
